8e6f0562a8c747c5d3b3d91c11e66e6f82d03bf4
Bryx28 <qjbfquebral@tip.edu.ph>
2021-11-13 01:19:51 +0000

    With Dockerfile

---
+++ design_app.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp design_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY design_app.py /home/myapp/" >> tempdir/Dockerfile
+
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/design_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t webapp .
+
+docker run -t -d -p 8080:8080 --name webapprunning webapp
+
+docker ps -a
